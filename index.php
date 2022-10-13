5315f731682e7b9100466dea39742c69be922857
piokaf05@gmail.com <piokaf05@gmail.com>
2022-10-13 22:13:04 +0200

    Basic page content added

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>SQL Admin</title>
+</head>
+<body>
+    <?php
+        if(!isset($_COOKIE['host']) || !isset($_COOKIE['user']) || !isset($_COOKIE['password']) || !isset($_COOKIE['db'])) {
+            header("Location: login.php");
+        }
+    ?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>SQL Admin - Login</title>
+    <link rel="stylesheet" href="styles/styles-login.css">
+</head>
+<body>
+    <div id="container"></div>
+</body>
+</html>
