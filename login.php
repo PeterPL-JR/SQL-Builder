f7dd5b62a96a73ef832bc95b75d145ae5c00bd77
piokaf05@gmail.com <piokaf05@gmail.com>
2022-10-15 13:25:27 +0200

    New logo style on the login page

--- login.php
+++ login.php
@@ -9,8 +9,9 @@
     <div id="container">
         <div id="logo">
             <img src="images/icon.png">
-            <div><span>SQL Builder</span> - Login</div>
+            <div><span>SQL Builder</span></div>
         </div>
+        <div id="login-label">Login</div>
     </div>
 </body>
 </html>
