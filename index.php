26000525f35bc4322a4ea6db4fee85665c1deb97
piokaf05@gmail.com <piokaf05@gmail.com>
2022-10-15 15:45:22 +0200

    Database connection PHP script moved to the top of file

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+    if(!isset($_COOKIE['server']) || !isset($_COOKIE['username']) || !isset($_COOKIE['database'])) {
+        header("Location: login.php");
+        return;
+    }
+    
+    $server = $_COOKIE['server'];
+    $username = $_COOKIE['username'];
+    $database = $_COOKIE['database'];
+    $password = (isset($_COOKIE['password'])) ? $_COOKIE['password'] : "";
+
+    $connect = mysqli_connect($server, $username, $password, $database);
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -5,18 +19,6 @@
     <title>SQL Builder</title>
 </head>
 <body>
-    <?php
-        if(!isset($_COOKIE['server']) || !isset($_COOKIE['username']) || !isset($_COOKIE['database'])) {
-            header("Location: login.php");
-            return;
-        }
-
-        $server = $_COOKIE['server'];
-        $username = $_COOKIE['username'];
-        $database = $_COOKIE['database'];
-        $password = (isset($_COOKIE['password'])) ? $_COOKIE['password'] : "";
-
-        $connect = mysqli_connect($server, $username, $password, $database);
-    ?>
+    
 </body>
 </html>
